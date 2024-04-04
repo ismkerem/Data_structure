045c170fc08fd643fac90e0543df29c03f5d881f
İsmail kerem Gül <ismkerem61@gmail.com>
2024-04-04 11:27:11 +0300

    tek yönlü bağlı liste eklendi.

---
+++ Data_structure/linked_list.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+using namespace std;
+class dugum {
+private:
+	int data;
+	dugum* sonraki;
+public:
+	dugum(int data) {
+		this.data = data;
+		sonraki = nullptr;
+	}
+	dugum(){}
+	void basaEkle(int veri) {
+		dugum* yeniDugum = new dugum;
+		yeniDugum->data = veri; 
+		yeniDugum->sonraki = head; 
+		head = yeniDugum; 
+	}
+	void sonaEkle(int veri) {
+		dugum* yeniDugum = new dugum;
+		yeniDugum->data = veri;
+		yeniDugum->next = nullptr;
+
+		if (head == nullptr) {
+			head = yeniDugum;
+		}
+		else {
+			dugum* temp = head;
+			while (temp->sonraki != nullptr) {
+				temp = temp->sonraki;
+			}
+			temp->sonraki = yeniDugum;
+		}
+	}
+	void eleman_ekle(int aranan_veri,dugum* eklenmek_istenen) {
+		
+		dugum* temp = head;
+		while (temp->sonraki !=nullptr)
+		{
+			if (temp->data == aranan_veri) {
+				break
+			}
+			temp->sonraki = temp;
+		}
+		dugum* temp2 = temp;
+		temp->sonraki = eklenmek_istenen;
+		eklenmek_istenen->sonraki = temp2->sonraki;
+	}
+	void eleman_sil(int data,dugum* sil) {
+		dugum* temp = sil;
+		while (temp->sonraki!=nullptr)
+		{
+			if (temp->sonraki->data = data) {
+				temp->sonraki = temp->sonraki->sonraki;
+				break
+			}
+			else
+			{
+				temp->sonraki = temp;
+			}
+		}
+
+}
+	void yazdir() {
+		dugum* temp = head;
+		while (temp != nullptr) {
+			cout << temp->data << " ";
+			temp = temp->sonraki;
+		}
+		cout << endl;
+	}
+
+
+
+
+};
+
+int main() {
+	dugum liste; 
+
+	dugum.basaEkle(10);
+	dugum.basaEkle(20);
+	dugum.sonaEkle(30);
+
+	return 0;
+
+}
---
+++ linked_list.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        next = nullptr;
+    }
+
+   
+    void addNode(int data) {
+        Node* newNode = new Node(data);
+        if (next == nullptr) {
+            next = newNode;
+        }
+        else {
+           
+            Node* temp = next;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+
+    void deleteNode(int data) {
+        Node* prev = this;
+        Node* curr = next;
+
+       
+        while (curr != nullptr && curr->data != data) {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        
+        if (curr != nullptr) {
+           
+            if (prev == this) {
+                next = curr->next;
+            }
+            else {
+               
+                prev->next = curr->next;
+            }
+            
+            delete curr;
+        }
+    }
+}
+
+int main() {
+
+
+
+
+
+	return 0;
+}
